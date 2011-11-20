421185d6effa711dff6763192db0e5079ef7f56e
Joachim <jspam@web.de>
2011-11-20 22:03:20 +0100

    [prototype] Add unit test so that the automated build process is happy

---
+++ prototype/org.xtext.example.mydsl.tests/src/org/xtext/example/mydsl/tests/TestTest.java
@@ -0,0 +1,14 @@
+package org.xtext.example.mydsl.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}

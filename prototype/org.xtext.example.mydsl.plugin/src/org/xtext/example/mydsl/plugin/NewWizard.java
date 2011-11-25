fb0f692708a81981d7e58c86fc8fe0e276b62ed6
Joachim <jspam@web.de>
2011-11-26 00:02:50 +0100

    [prototype] Add New Wizard (really, this time ...)

---
+++ prototype/org.xtext.example.mydsl.plugin/src/org/xtext/example/mydsl/plugin/NewWizard.java
@@ -0,0 +1,26 @@
+package org.xtext.example.mydsl.plugin;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NewWizard extends Wizard implements INewWizard {
+
+	public NewWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}

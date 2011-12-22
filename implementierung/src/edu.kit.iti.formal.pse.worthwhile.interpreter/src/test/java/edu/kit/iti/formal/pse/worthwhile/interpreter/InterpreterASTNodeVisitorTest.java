e87a19ca5b8a7897d7ba24f3270c2731c0f18e45
Leon Handreke <leonh@ndreke.de>
2011-12-22 02:05:44 +0100

    [impl:interpreter] Add more useful little default test

---
+++ implementierung/src/edu.kit.iti.formal.pse.worthwhile.interpreter/src/test/java/edu/kit/iti/formal/pse/worthwhile/interpreter/InterpreterASTNodeVisitorTest.java
@@ -0,0 +1,16 @@
+package edu.kit.iti.formal.pse.worthwhile.interpreter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class InterpreterASTNodeVisitorTest {
+
+	@Test
+	public void test() {
+		InterpreterASTNodeVisitor v = new InterpreterASTNodeVisitor();
+		assertNotNull(v);
+	}
+
+}

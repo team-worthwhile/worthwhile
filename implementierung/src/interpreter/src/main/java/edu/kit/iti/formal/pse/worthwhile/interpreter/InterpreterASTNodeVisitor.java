7669cb4ecd8711a0521460702f3e7a36f5a66a9e
Joachim <jspam@web.de>
2011-12-23 01:08:48 +0100

    [impl:interpreter] Remove unused import

--- implementierung/src/interpreter/src/main/java/edu/kit/iti/formal/pse/worthwhile/interpreter/InterpreterASTNodeVisitor.java
+++ implementierung/src/interpreter/src/main/java/edu/kit/iti/formal/pse/worthwhile/interpreter/InterpreterASTNodeVisitor.java
@@ -42,7 +42,6 @@ import edu.kit.iti.formal.pse.worthwhile.model.ast.Minus;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Modulus;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Multiplication;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Negation;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Parameter;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Plus;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Postcondition;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Precondition;

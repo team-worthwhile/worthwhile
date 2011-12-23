0764d917c86cfac2f9181c9c7e0418fe2561e2c6
Joachim <jspam@web.de>
2011-12-23 15:45:12 +0100

    [impl] Delete leftover files

--- implementierung/src/edu.kit.iti.formal.pse.worthwhile.ui/src/edu/kit/iti/formal/pse/worthwhile/ui/wizard/WorthwhileProjectInfo.java
+++
@@ -1,7 +0,0 @@
-package edu.kit.iti.formal.pse.worthwhile.ui.wizard;
-
-import org.eclipse.xtext.ui.wizard.DefaultProjectInfo;
-
-public class WorthwhileProjectInfo extends DefaultProjectInfo {
-	
-}

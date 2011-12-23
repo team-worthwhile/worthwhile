fcaa9d8e1aa0070368c5faa68b0b29e22c288bcc
Leon Handreke <leonh@ndreke.de>
2011-12-23 20:15:23 +0100

    [impl] Make ASTNodeVisitor visit method public

--- implementierung/src/worthwhile.model/src/edu/kit/iti/formal/pse/worthwhile/model/ast/visitor/ASTNodeVisitor.java
+++ implementierung/src/worthwhile.model/src/edu/kit/iti/formal/pse/worthwhile/model/ast/visitor/ASTNodeVisitor.java
@@ -8,7 +8,8 @@ import edu.kit.iti.formal.pse.worthwhile.model.ast.ASTNode;
  * @see edu.kit.iti.formal.pse.worthwhile.model.ast.AstPackage#getASTNodeVisitor()
  */
 public abstract class ASTNodeVisitor {
-	void visit(ASTNode node) throws Exception {
-		throw new Exception("Visitor cannot handle this type of ASTNode!");
+	public void visit(ASTNode node) {
+		throw new RuntimeException(
+				"Visitor cannot handle this type of ASTNode!");
 	}
 }

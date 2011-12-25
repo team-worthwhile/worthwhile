a7740bc90cfe988455842caa9f516564e4ffa1ef
Joachim <jspam@web.de>
2011-12-25 10:30:42 +0100

    [impl:help] Make dynamic help in editor work

--- implementierung/src/worthwhile.ui/src/main/java/edu/kit/iti/formal/pse/worthwhile/ui/editor/WorthwhileEditorContextProvider.java
+++ implementierung/src/worthwhile.ui/src/main/java/edu/kit/iti/formal/pse/worthwhile/ui/editor/WorthwhileEditorContextProvider.java
@@ -3,6 +3,7 @@ package edu.kit.iti.formal.pse.worthwhile.ui.editor;
 import org.eclipse.help.HelpSystem;
 import org.eclipse.help.IContext;
 import org.eclipse.help.IContextProvider;
+import org.eclipse.help.internal.HelpPlugin;
 
 /**
  * Provides help context IDs for a {@link WorthwhileEditor} depending on the
@@ -46,12 +47,18 @@ public class WorthwhileEditorContextProvider implements IContextProvider {
 
     @Override
     public final IContext getContext(final Object target) {
-	String currentKeyword = editor.getCurrentKeyword(); 
-	
+	// Get the context for the keyword the cursor is currently on. If there
+	// is no such keyword, return null which displays the generic help for
+	// the editor.
+	String currentKeyword = editor.getCurrentKeyword();
+
 	if (currentKeyword != "") {
-		return HelpSystem.getContext("keyword." + currentKeyword);
+	    return HelpSystem
+		    .getContext("edu.kit.iti.formal.pse.worthwhile.help.keyword_"
+			    + currentKeyword);
 	}
-       return HelpSystem.getContext("editor");
+
+	return null;
     }
 
     @Override

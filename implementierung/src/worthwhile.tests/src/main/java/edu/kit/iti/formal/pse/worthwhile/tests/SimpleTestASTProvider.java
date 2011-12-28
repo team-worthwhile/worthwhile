ffb0dd12ac4c972d73175136f7d8ea2e41c6f194
Leon Handreke <leonh@ndreke.de>
2011-12-28 12:50:31 +0100

    [impl] Make prover and interpreter OSGi bundles

--- implementierung/src/worthwhile.tests/src/main/java/edu/kit/iti/formal/pse/worthwhile/tests/SimpleTestASTProvider.java
+++ implementierung/src/worthwhile.tests/src/main/java/edu/kit/iti/formal/pse/worthwhile/tests/SimpleTestASTProvider.java
@@ -9,12 +9,13 @@ import com.google.inject.Injector;
 
 import edu.kit.iti.formal.pse.worthwhile.WorthwhileStandaloneSetup;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.ASTNode;
+import edu.kit.iti.formal.pse.worthwhile.model.ast.Expression;
 
 public class SimpleTestASTProvider {
 
-	public ASTNode getSimpleTestAST() {
+	public static ASTNode getSimpleTestAST() {
 		// from http://wiki.pse.ndreke.de/proof_of_concept_ast_output
-		String testString = "function Boolean func(Integer param1, Boolean param2) {\nBoolean ret := true\nInteger D:= 42\nret := param1 || param2\nreturn ret\n}\n{\nBoolean b := false\nInteger a := 4\nInteger b := 45\n}";
+		String testString = "function Boolean func(Integer param1, Boolean param2) {\nBoolean ret := true\nInteger D:= 42\nret := param1 || param2\nreturn ret\n}\n{\nBoolean b := false\nInteger a := 4\nInteger b := 45\nBool trueTestFormula = ((true || false) && true)\nBool falseTestFormula = ((true || false) && false)}";
 		Injector guiceInjector = new WorthwhileStandaloneSetup()
 				.createInjectorAndDoEMFRegistration();
 		IParser parser = guiceInjector.getInstance(IParser.class);
@@ -25,6 +26,11 @@ public class SimpleTestASTProvider {
 		// Iterable<INode> errors = result.getSyntaxErrors();
 		ASTNode root = (ASTNode) result.getRootASTElement();
 		return root;
+	}
 
+	public static Expression getSimpleTrueFormula() {
+		ASTNode n = SimpleTestASTProvider.getSimpleTestAST();
+		n.getLineNumber();
+		return (Expression) n;
 	}
 }

68f9cb2105261f5279997b39c21504c0d74ddf81
Leon Handreke <leonh@ndreke.de>
2011-12-28 14:59:41 +0100

    [impl] Add workaround for model resolving bug in unit tests
    Stolen from http://www.eclipse.org/forums/index.php/m/735764/

--- implementierung/src/worthwhile/src/main/java/edu/kit/iti/formal/pse/worthwhile/WorthwhileStandaloneSetup.java
+++ implementierung/src/worthwhile/src/main/java/edu/kit/iti/formal/pse/worthwhile/WorthwhileStandaloneSetup.java
@@ -1,13 +1,23 @@
 package edu.kit.iti.formal.pse.worthwhile;
 
+import com.google.inject.Injector;
+
+import edu.kit.iti.formal.pse.worthwhile.model.ast.impl.AstPackageImpl;
+
 /**
  * Initialization support for running Xtext languages without equinox extension
  * registry.
  */
 public class WorthwhileStandaloneSetup extends
-	WorthwhileStandaloneSetupGenerated {
+WorthwhileStandaloneSetupGenerated {
 
-    public static void doSetup() {
-	new WorthwhileStandaloneSetup().createInjectorAndDoEMFRegistration();
-    }
+	public static void doSetup() {
+		new WorthwhileStandaloneSetup().createInjectorAndDoEMFRegistration();
+	}
+
+	@Override
+	public Injector createInjectorAndDoEMFRegistration() {
+		AstPackageImpl.init();
+		return super.createInjectorAndDoEMFRegistration();
+	}
 }

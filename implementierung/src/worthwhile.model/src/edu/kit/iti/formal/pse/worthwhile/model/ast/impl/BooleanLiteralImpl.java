af79f683b2f227c30a9adc157cc0591bdd167131
Leon Handreke <leonh@ndreke.de>
2011-12-30 13:12:20 +0100

    [impl:model] Reindent and clean up BooleanLiteralImpl

--- implementierung/src/worthwhile.model/src/edu/kit/iti/formal/pse/worthwhile/model/ast/impl/BooleanLiteralImpl.java
+++ implementierung/src/worthwhile.model/src/edu/kit/iti/formal/pse/worthwhile/model/ast/impl/BooleanLiteralImpl.java
@@ -6,165 +6,98 @@
  */
 package edu.kit.iti.formal.pse.worthwhile.model.ast.impl;
 
-import edu.kit.iti.formal.pse.worthwhile.model.ast.AstPackage;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.visitor.ASTNodeVisitor;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.BooleanLiteral;
-
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.ecore.EClass;
-
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 
+import edu.kit.iti.formal.pse.worthwhile.model.ast.AstPackage;
+import edu.kit.iti.formal.pse.worthwhile.model.ast.BooleanLiteral;
+import edu.kit.iti.formal.pse.worthwhile.model.ast.visitor.ASTNodeVisitor;
+
 /**
- * <!-- begin-user-doc -->
- * An implementation of the model object '<em><b>Boolean Literal</b></em>'.
- * <!-- end-user-doc -->
- * <p>
- * The following features are implemented:
- * <ul>
- *   <li>{@link edu.kit.iti.formal.pse.worthwhile.model.ast.impl.BooleanLiteralImpl#isValue <em>Value</em>}</li>
- * </ul>
- * </p>
- *
- * @generated
+ * A literal representin a Boolean value
  */
 public class BooleanLiteralImpl extends LiteralImpl implements BooleanLiteral {
 	/**
-	 * The default value of the '{@link #isValue() <em>Value</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #isValue()
-	 * @generated
-	 * @ordered
+	 * Default value of the literal
 	 */
 	protected static final boolean VALUE_EDEFAULT = false;
 
-	/**
-	 * The cached value of the '{@link #isValue() <em>Value</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see #isValue()
-	 * @generated
-	 * @ordered
-	 */
 	protected boolean value = VALUE_EDEFAULT;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	protected BooleanLiteralImpl() {
 		super();
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	protected EClass eStaticClass() {
 		return AstPackage.Literals.BOOLEAN_LITERAL;
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+	@Override
 	public boolean isValue() {
-		return value;
+		return this.value;
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+	@Override
 	public void setValue(boolean newValue) {
-		boolean oldValue = value;
-		value = newValue;
-		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, AstPackage.BOOLEAN_LITERAL__VALUE, oldValue, value));
+		boolean oldValue = this.value;
+		this.value = newValue;
+		if (this.eNotificationRequired())
+			this.eNotify(new ENotificationImpl(this, Notification.SET, AstPackage.BOOLEAN_LITERAL__VALUE, oldValue, this.value));
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
 		switch (featureID) {
-			case AstPackage.BOOLEAN_LITERAL__VALUE:
-				return isValue();
+		case AstPackage.BOOLEAN_LITERAL__VALUE:
+			return this.isValue();
 		}
 		return super.eGet(featureID, resolve, coreType);
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public void eSet(int featureID, Object newValue) {
 		switch (featureID) {
-			case AstPackage.BOOLEAN_LITERAL__VALUE:
-				setValue((Boolean)newValue);
-				return;
+		case AstPackage.BOOLEAN_LITERAL__VALUE:
+			this.setValue((Boolean)newValue);
+			return;
 		}
 		super.eSet(featureID, newValue);
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public void eUnset(int featureID) {
 		switch (featureID) {
-			case AstPackage.BOOLEAN_LITERAL__VALUE:
-				setValue(VALUE_EDEFAULT);
-				return;
+		case AstPackage.BOOLEAN_LITERAL__VALUE:
+			this.setValue(VALUE_EDEFAULT);
+			return;
 		}
 		super.eUnset(featureID);
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public boolean eIsSet(int featureID) {
 		switch (featureID) {
-			case AstPackage.BOOLEAN_LITERAL__VALUE:
-				return value != VALUE_EDEFAULT;
+		case AstPackage.BOOLEAN_LITERAL__VALUE:
+			return this.value != VALUE_EDEFAULT;
 		}
 		return super.eIsSet(featureID);
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	@Override
 	public String toString() {
-		if (eIsProxy()) return super.toString();
+		if (this.eIsProxy()) return super.toString();
 
 		StringBuffer result = new StringBuffer(super.toString());
 		result.append(" (value: ");
-		result.append(value);
+		result.append(this.value);
 		result.append(')');
 		return result.toString();
 	}
 
-    public void accept(ASTNodeVisitor visitor) {
-        visitor.visit(this);
-    }
+	@Override
+	public void accept(ASTNodeVisitor visitor) {
+		visitor.visit(this);
+	}
 } //BooleanLiteralImpl

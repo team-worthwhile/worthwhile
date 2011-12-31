0b71fc78ab3e44c9b0454c9713debb1822005157
Leon Handreke <leonh@ndreke.de>
2011-12-31 12:53:32 +0100

    [impl:prover] Document Validity and Satisfiability

--- implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/FormulaSatisfiability.java
+++ implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/FormulaSatisfiability.java
@@ -3,22 +3,12 @@
  */
 package edu.kit.iti.formal.pse.worthwhile.prover;
 
-/** 
- * 
+/**
+ * Expresses the satisfiability of a formula. A formula is satisfiable if there
+ * exists an interpretation of the formula so that it is true.
  */
 public enum FormulaSatisfiability {
-	/** 
-	 * 
-	 */
 	SATISFIABLE,
-	
-	/** 
-	 * 
-	 */
 	UNSATISFIABLE,
-	
-	/** 
-	 * 
-	 */
 	UNKOWN
 }
--- implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/Validity.java
+++ implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/Validity.java
@@ -3,22 +3,12 @@
  */
 package edu.kit.iti.formal.pse.worthwhile.prover;
 
-/** 
- * 
+/**
+ * Expresses the validity of a formula. A formula is valid if (not formula) is
+ * unsatisfiable, i.e. the formula is true for all possible interpretations.
  */
 public enum Validity {
-	/** 
-	 * 
-	 */
 	VALID,
-	
-	/** 
-	 * 
-	 */
 	INVALID,
-	
-	/** 
-	 * 
-	 */
 	UNKNOWN
 }

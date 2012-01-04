852f309dc03f135c7eee1dc4f5eb8ef753bea644
Fabian <bafain@gmail.com>
2012-01-04 14:48:38 +0100

    [impl:common.tests] Apply Worthwhile formatter (whitespace only)

--- implementierung/src/worthwhile.common.tests/src/edu/kit/iti/formal/pse/worthwhile/common/tests/TestASTProvider.java
+++ implementierung/src/worthwhile.common.tests/src/edu/kit/iti/formal/pse/worthwhile/common/tests/TestASTProvider.java
@@ -18,50 +18,45 @@ import edu.kit.iti.formal.pse.worthwhile.model.ast.VariableDeclaration;
 
 public class TestASTProvider {
 
-	public static Program getRootASTNode(String parseText) {
-		// from http://wiki.pse.ndreke.de/proof_of_concept_ast_output
-		Injector guiceInjector = new WorthwhileStandaloneSetup()
-		.createInjectorAndDoEMFRegistration();
-		IParser parser = guiceInjector.getInstance(IParser.class);
+    public static Program getRootASTNode(String parseText) {
+	// from http://wiki.pse.ndreke.de/proof_of_concept_ast_output
+	Injector guiceInjector = new WorthwhileStandaloneSetup().createInjectorAndDoEMFRegistration();
+	IParser parser = guiceInjector.getInstance(IParser.class);
 
-		IParseResult result = parser.parse(new StringReader(parseText));
+	IParseResult result = parser.parse(new StringReader(parseText));
 
-		// System.out.println("Getting syntax errors ...");
-		// Iterable<INode> errors = result.getSyntaxErrors();
-		Program root = null;
-		
-		if (!result.hasSyntaxErrors()) {
-		    root = (Program) result.getRootASTElement();
-		}
-		
-		return root;
+	// System.out.println("Getting syntax errors ...");
+	// Iterable<INode> errors = result.getSyntaxErrors();
+	Program root = null;
+
+	if (!result.hasSyntaxErrors()) {
+	    root = (Program) result.getRootASTElement();
 	}
 
-	/**
-	 * @return A simple, human-readable formula that evaluates to true (i.e. is
-	 *         always satisfiable) and covers a lot of the constructs supported
-	 *         by the language
-	 */
-	public static Expression getTestFormula() {
-		return parseFormulaString("((!false || ((9 % 4) = 1)) && true && ((3 + 3) = 6)");
+	return root;
+    }
+
+    /**
+     * @return A simple, human-readable formula that evaluates to true (i.e. is always satisfiable) and covers a lot of
+     *         the constructs supported by the language
+     */
+    public static Expression getTestFormula() {
+	return parseFormulaString("((!false || ((9 % 4) = 1)) && true && ((3 + 3) = 6)");
+    }
+
+    public static Expression getNegatedTestFormula() {
+	return parseFormulaString("!((!false || ((9 % 4) = 1)) && true && ((3 + 3) = 6)");
+    }
+
+    public static Expression parseFormulaString(String formulaString) {
+	ASTNode n = TestASTProvider.getRootASTNode("{\nBoolean x := " + formulaString + "\n" + "}\n");
+
+	if (n == null) {
+	    return null;
 	}
 
-	public static Expression getNegatedTestFormula() {
-		return parseFormulaString("!((!false || ((9 % 4) = 1)) && true && ((3 + 3) = 6)");
-	}
+	List<Statement> statements = ((Block) ((Program) n).getMainBlock().getStatements().get(0)).getStatements();
 
-	public static Expression parseFormulaString(String formulaString) {
-		ASTNode n = TestASTProvider.getRootASTNode("{\nBoolean x := " +
-				formulaString + "\n" +
-				"}\n");
-
-		if (n == null) {
-		    return null;
-		}
-
-		List<Statement> statements = ((Block) ((Program) n).getMainBlock()
-				.getStatements().get(0)).getStatements();
-
-		return ((VariableDeclaration) (statements.get(0))).getInitialValue();
-	}
+	return ((VariableDeclaration) (statements.get(0))).getInitialValue();
+    }
 }

9de59960353090bcbd676c81c056b8181cbe64a8
Fabian <bafain@gmail.com>
2012-01-05 00:30:06 +0100

    [impl:prover] Remove Expression visit methods
    - expressions are not executed, i.e. do not change program state, and
      therefore not transformed

--- implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/WPStrategy.java
+++ implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/WPStrategy.java
@@ -1,50 +1,20 @@
 package edu.kit.iti.formal.pse.worthwhile.prover;
 
-import edu.kit.iti.formal.pse.worthwhile.model.ast.ASTNode;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Addition;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.ArrayAccess;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.ArrayLength;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.ArrayLiteral;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.ArrayType;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Assertion;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Assignment;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Assumption;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Axiom;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Block;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.BooleanLiteral;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.BooleanType;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Conditional;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Conjunction;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Disjunction;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Division;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Equal;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Equivalence;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.ExistsQuantifier;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Expression;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.ForAllQuantifier;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.FunctionCall;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.FunctionDeclaration;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Greater;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.GreaterOrEqual;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Implication;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.IntegerLiteral;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.IntegerType;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Invariant;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Less;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.LessOrEqual;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Loop;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Minus;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Modulus;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Multiplication;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Negation;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Plus;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Postcondition;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Precondition;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Program;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.QuantifiedExpression;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.ReturnStatement;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Subtraction;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.Unequal;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.VariableDeclaration;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.VariableReference;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.visitor.ASTNodeVisitor;
@@ -77,56 +47,6 @@ class WPStrategy extends ASTNodeVisitor implements FormulaGenerator {
 	// begin-user-code
 	// TODO Auto-generated method stub
 	return null;
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Addition addition)
-     */
-    public void visit(Addition addition) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(ArrayAccess arrayAccess)
-     */
-    public void visit(ArrayAccess arrayAccess) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(ArrayLength arrayLength)
-     */
-    public void visit(ArrayLength arrayLength) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(ArrayLiteral arrayLiteral)
-     */
-    public void visit(ArrayLiteral arrayLiteral) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(ArrayType arrayType)
-     */
-    public void visit(ArrayType arrayType) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
 	// end-user-code
     }
 
@@ -181,109 +101,9 @@ class WPStrategy extends ASTNodeVisitor implements FormulaGenerator {
     }
 
     /**
-     * @see ASTNodeVisitor#visit(BooleanLiteral booleanLiteral)
-     */
-    public void visit(BooleanLiteral booleanLiteral) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(BooleanType booleanType)
-     */
-    public void visit(BooleanType booleanType) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
      * @see ASTNodeVisitor#visit(Conditional conditional)
      */
     public void visit(Conditional conditional) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Conjunction conjunction)
-     */
-    public void visit(Conjunction conjunction) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Disjunction disjunction)
-     */
-    public void visit(Disjunction disjunction) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Division division)
-     */
-    public void visit(Division division) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Equal equal)
-     */
-    public void visit(Equal equal) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Equivalence equivalence)
-     */
-    public void visit(Equivalence equivalence) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(ExistsQuantifier existsQuantifier)
-     */
-    public void visit(ExistsQuantifier existsQuantifier) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(ForAllQuantifier forAllQuantifier)
-     */
-    public void visit(ForAllQuantifier forAllQuantifier) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(FunctionCall functionCall)
-     */
-    public void visit(FunctionCall functionCall) {
 	// begin-user-code
 	// TODO Auto-generated method stub
 
@@ -301,56 +121,6 @@ class WPStrategy extends ASTNodeVisitor implements FormulaGenerator {
     }
 
     /**
-     * @see ASTNodeVisitor#visit(Greater greater)
-     */
-    public void visit(Greater greater) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(GreaterOrEqual greaterOrEqual)
-     */
-    public void visit(GreaterOrEqual greaterOrEqual) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Implication implication)
-     */
-    public void visit(Implication implication) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(IntegerLiteral integerLiteral)
-     */
-    public void visit(IntegerLiteral integerLiteral) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(IntegerType integerType)
-     */
-    public void visit(IntegerType integerType) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
      * @see ASTNodeVisitor#visit(Invariant invariant)
      */
     public void visit(Invariant invariant) {
@@ -361,90 +131,9 @@ class WPStrategy extends ASTNodeVisitor implements FormulaGenerator {
     }
 
     /**
-     * @see ASTNodeVisitor#visit(Less less)
-     */
-    public void visit(Less less) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(LessOrEqual lessOrEqual)
-     */
-    public void visit(LessOrEqual lessOrEqual) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
      * @see ASTNodeVisitor#visit(Loop loop)
      */
     public void visit(Loop loop) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Minus minus)
-     */
-    public void visit(Minus minus) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Modulus modulus)
-     */
-    public void visit(Modulus modulus) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Multiplication multiplication)
-     */
-    public void visit(Multiplication multiplication) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Negation negation)
-     */
-    public void visit(Negation negation) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(ASTNode node)
-     */
-    @Override
-    public void visit(ASTNode node) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Plus plus)
-     */
-    public void visit(Plus plus) {
 	// begin-user-code
 	// TODO Auto-generated method stub
 
@@ -472,15 +161,6 @@ class WPStrategy extends ASTNodeVisitor implements FormulaGenerator {
     }
 
     /**
-     * @see ASTNodeVisitor#visit(Predicates predicates)
-     */
-    /*
-     * public void visit(Predicates predicates) { // begin-user-code // TODO Auto-generated method stub
-     * 
-     * // end-user-code }
-     */
-
-    /**
      * @see ASTNodeVisitor#visit(Program program)
      */
     public void visit(Program program) {
@@ -491,39 +171,9 @@ class WPStrategy extends ASTNodeVisitor implements FormulaGenerator {
     }
 
     /**
-     * @see ASTNodeVisitor#visit(QuantifiedExpression quantifiedExpression)
-     */
-    public void visit(QuantifiedExpression quantifiedExpression) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
      * @see ASTNodeVisitor#visit(ReturnStatement returnStatement)
      */
     public void visit(ReturnStatement returnStatement) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Subtraction subtraction)
-     */
-    public void visit(Subtraction subtraction) {
-	// begin-user-code
-	// TODO Auto-generated method stub
-
-	// end-user-code
-    }
-
-    /**
-     * @see ASTNodeVisitor#visit(Unequal unequal)
-     */
-    public void visit(Unequal unequal) {
 	// begin-user-code
 	// TODO Auto-generated method stub
 

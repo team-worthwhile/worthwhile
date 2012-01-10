9a9a867c9601618a4cc8ccb74f39bc5081690c31
Joachim <jspam@web.de>
2012-01-10 15:37:49 +0100

    [tests] Throw exception if program string contains parser errors

--- implementierung/src/worthwhile.common.tests/src/edu/kit/iti/formal/pse/worthwhile/common/tests/TestASTProvider.java
+++ implementierung/src/worthwhile.common.tests/src/edu/kit/iti/formal/pse/worthwhile/common/tests/TestASTProvider.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.Resource.Diagnostic;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
 
@@ -24,7 +25,8 @@ public class TestASTProvider {
 
 	public static Program getRootASTNode(String parseText) {
 		// from http://wiki.pse.ndreke.de/proof_of_concept_ast_output
-		// see also http://wiki.eclipse.org/Xtext/FAQ#How_do_I_load_my_model_in_a_standalone_Java_application.C2.A0.3F
+		// see also
+		// http://wiki.eclipse.org/Xtext/FAQ#How_do_I_load_my_model_in_a_standalone_Java_application.C2.A0.3F
 		Injector guiceInjector = new WorthwhileStandaloneSetup().createInjectorAndDoEMFRegistration();
 		XtextResourceSet resourceSet = guiceInjector.getInstance(XtextResourceSet.class);
 		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
@@ -37,19 +39,25 @@ public class TestASTProvider {
 		}
 
 		// System.out.println("Getting syntax errors ...");
-		// Iterable<Diagnostic> errors = resource.getErrors();
 		Program root = null;
 
 		if (resource.getErrors().isEmpty()) {
 			root = (Program) resource.getContents().get(0);
-		}
+		} else {
+			StringBuilder errorStringBuilder = new StringBuilder();
 
+			for (Diagnostic diag : resource.getErrors()) {
+				errorStringBuilder.append("\n Line " + diag.getLine() + ": " + diag.getMessage());
+			}
+
+			throw new IllegalArgumentException("Program contains errors:" + errorStringBuilder.toString());
+		}
 		return root;
 	}
 
 	/**
-	 * @return A simple, human-readable formula that evaluates to true (i.e. is always satisfiable) and covers a lot of
-	 *         the constructs supported by the language
+	 * @return A simple, human-readable formula that evaluates to true (i.e. is always satisfiable) and covers a lot
+	 *         of the constructs supported by the language
 	 */
 	public static Expression getTestFormula() {
 		return parseFormulaString("((!false || ((9 % 4) = 1)) && true && ((3 + 3) = 6)");
@@ -60,13 +68,14 @@ public class TestASTProvider {
 	}
 
 	public static Expression parseFormulaString(String formulaString) {
-		ASTNode n = TestASTProvider.getRootASTNode("{\nBoolean x := " + formulaString + "\n" + "}\n");
+		ASTNode n = TestASTProvider.getRootASTNode("{\nBoolean x := (" + formulaString + ")\n" + "}\n");
 
 		if (n == null) {
 			return null;
 		}
 
-		List<Statement> statements = ((Block) ((Program) n).getMainBlock().getStatements().get(0)).getStatements();
+		List<Statement> statements = ((Block) ((Program) n).getMainBlock().getStatements().get(0))
+		                .getStatements();
 
 		return ((VariableDeclaration) (statements.get(0))).getInitialValue();
 	}

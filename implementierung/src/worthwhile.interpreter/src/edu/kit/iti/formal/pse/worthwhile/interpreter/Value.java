45d9f9b2b1b9a79becbb07c63a17da6cd45a0d73
Stefan <sdderstefn@googlemail.com>
2012-01-11 20:56:19 +0100

    [interpreter] Implement the Value class.

--- implementierung/src/worthwhile.interpreter/src/edu/kit/iti/formal/pse/worthwhile/interpreter/Value.java
+++ implementierung/src/worthwhile.interpreter/src/edu/kit/iti/formal/pse/worthwhile/interpreter/Value.java
@@ -4,83 +4,173 @@
 package edu.kit.iti.formal.pse.worthwhile.interpreter;
 
 import java.math.BigInteger;
-import java.util.Set;
 
 /** 
- * 
+ * This class represents a value with its corresponding name to be used in the interpreter.
  */
 public class Value {
 	/** 
-	 * 
+	 * The type of the value.
 	 */
 	private ValueType valueType;
-
+	
 	/**
-	 * @return the valueType
+	 * The value of the integer.
+	 */
+	private BigInteger integerValue;
+	
+	/**
+	 * The value of the boolean.
+	 */
+	private Boolean booleanValue;
+	
+	/**
+	 * The value of the integer array.
+	 */
+	private BigInteger[] integerArray;
+	
+	/**
+	 * The value of the boolean array.
+	 */
+	private Boolean[] booleanArray;
+	
+	/**
+	 * This method constructs a Value of the type Integer {@link ValueType#INTEGER_TYPE}.
+	 * @param integerValue the value for this Value.
+	 */
+	public Value(BigInteger integerValue) {
+		this.setIntegerValue(integerValue);
+	}
+	
+	/**
+	 * This method constructs a Value of the type Boolean {@link ValueType#BOOLEAN_TYPE}.
+	 * @param booleanValue the value for this Value.
+	 */
+	public Value(Boolean booleanValue) {
+		this.setBooleanValue(booleanValue);
+	}
+	
+	/**
+	 * This method constructs a Value of the type Integer Array {@link ValueType#INTEGER_ARRAY_TYPE}.
+	 * @param integerArray the value for this array.
+	 */
+	public Value(BigInteger[] integerArray) {
+		this.setIntegerArray(integerArray);
+	}
+	
+	/**
+	 * This method constructs a Value of the type Boolean Array {@link ValueType#BOOLEAN_ARRAY_TYPE}.
+	 * @param booleanArray the value for this array.
+	 */
+	public Value(Boolean[] booleanArray) {
+		this.setBooleanArray(booleanArray);
+	}
+	
+	/**
+	 * This returns the type of this Value.
+	 * @return the valueType of this Value.
 	 */
 	public ValueType getValueType() {
-		// begin-user-code
 		return valueType;
-		// end-user-code
 	}
-
+	
 	/**
-	 * @param valueType
-	 *            the valueType to set
+	 * This sets the type of this Value {@link ValueType}.
+	 * @param valueType the type of this Value.
 	 */
-	public void setValueType(ValueType valueType) {
-		// begin-user-code
+	private void setValueType(ValueType valueType) {
 		this.valueType = valueType;
-		// end-user-code
 	}
 
 	/**
-	 * @return
-	 */
-	public ValueType getType() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
-	}
-
-	/**
-	 * @return
+	 * This method returns the integer value of this Value, if the Type of this Value is
+	 * {@link ValueType#INTEGER_TYPE}, otherwise it will return null.
+	 * @return returns the integer value of this Value or null.
 	 */
 	public BigInteger getIntegerValue() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		if(this.getValueType() == ValueType.INTEGER_TYPE) {
+			return this.integerValue;
+		} else {		
+			return null;
+		}
 	}
 
 	/**
-	 * @return
+	 * This method returns the boolean value of this Value, if the Type of this Value is
+	 * {@link ValueType#BOOLEAN_TYPE}, otherwise it will return null.
+	 * @return returns the boolean value of this Value or null.
 	 */
 	public Boolean getBooleanValue() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		if(this.getValueType() == ValueType.BOOLEAN_TYPE) {
+			return this.booleanValue;
+		} else {
+			return null;
+		}
 	}
 
 	/**
-	 * @return
+	 * This method returns the boolean array of this Value, if the Type of this Value is
+	 * {@link ValueType#BOOLEAN_ARRAY_TYPE}, otherwise it will return null.
+	 * @return returns the boolean array of this Value or null.
 	 */
-	public Set<Boolean> getBooleanArray() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+	public Boolean[] getBooleanArray() {
+		if(this.getValueType() == ValueType.BOOLEAN_ARRAY_TYPE) {
+			return this.booleanArray;
+		} else {
+			return null;
+		}
 	}
 
 	/**
-	 * @return
+	 * This method returns the integer array of this Value, if the Type of this Value is
+	 * {@link ValueType#INTEGER_ARRAY_TYPE}, otherwise it will return null.
+	 * @return returns the integer array of this Value or null.
 	 */
-	public Set<Integer> getIntegerArray() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+	public BigInteger[] getIntegerArray() {
+		if(this.getValueType() == ValueType.INTEGER_ARRAY_TYPE) {
+			return this.integerArray;
+		} else {
+			return null;
+		}
 	}
+
+	/**
+	 * This sets the integer value of this Value.
+	 * Note, that by calling this function, the type of this Value will be set to {@link ValueType.INTEGER_TYPE}.
+	 * @param integerValue the integer value to be set.
+	 */
+	public void setIntegerValue(BigInteger integerValue) {
+		this.setValueType(ValueType.INTEGER_TYPE);
+        	this.integerValue = integerValue;
+        }
+
+	/**
+	 * This sets the boolean value of this Value.
+	 * Note, that by calling this function, the type of this Value will be set to {@link ValueType.BOOLEAN_TYPE}.
+	 * @param booleanValue the boolean value to be set.
+	 */
+	public void setBooleanValue(Boolean booleanValue) {
+		this.setValueType(ValueType.BOOLEAN_TYPE);
+        	this.booleanValue = booleanValue;
+        }
+
+	/**
+	 * This sets the integer array of this Value.
+	 * Note, that by calling this function, the type of this Value will be set to {@link ValueType.INTEGER_ARRAY_TYPE}.
+	 * @param integerArray the integer array to be set.
+	 */
+	public void setIntegerArray(BigInteger[] integerArray) {
+		this.setValueType(ValueType.INTEGER_ARRAY_TYPE);
+        	this.integerArray = integerArray;
+        }
+
+	/**
+	 * This sets the boolean array of this Value.
+	 * Note, that by calling this function, the type of this Value will be set to {@link ValueType.BOOLEAN_ARRAY_TYPE}.
+	 * @param booleanArray the boolean array to be set.
+	 */
+	public void setBooleanArray(Boolean[] booleanArray) {
+		this.setValueType(ValueType.BOOLEAN_ARRAY_TYPE);
+        	this.booleanArray = booleanArray;
+        }
 }

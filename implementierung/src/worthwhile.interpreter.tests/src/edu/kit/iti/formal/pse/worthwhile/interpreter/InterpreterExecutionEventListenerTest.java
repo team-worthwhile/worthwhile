c9b5ef657bd4938f5f3ba2c76f9039123fb75ba0
Stefan <sdderstefn@googlemail.com>
2012-01-14 18:11:49 +0100

    [interpreter.tests] Complete test for method statementExecuted() and statementWillExecute() so far

--- implementierung/src/worthwhile.interpreter.tests/src/edu/kit/iti/formal/pse/worthwhile/interpreter/InterpreterExecutionEventListenerTest.java
+++ implementierung/src/worthwhile.interpreter.tests/src/edu/kit/iti/formal/pse/worthwhile/interpreter/InterpreterExecutionEventListenerTest.java
@@ -11,6 +11,7 @@ import edu.kit.iti.formal.pse.worthwhile.model.ast.Statement;
 public class InterpreterExecutionEventListenerTest {
 	
 	public boolean test;
+	public int counter;
 	Interpreter interpreter;
 	
 	@Before
@@ -18,19 +19,96 @@ public class InterpreterExecutionEventListenerTest {
 		this.test = false;
 	}
 	
+	private void testStatements(AbstractExecutionEventListener listener) {
+		//Boolean VariableDeclaration
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("Boolean a := true\n")), listener);
+		assertFalse(this.test);
+		this.interpreter.execute();
+		assertTrue(this.test);
+		
+		//Integer VariableDeclaration
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("Integer a := 5\n")), listener);
+		assertFalse(this.test);
+		this.interpreter.execute();
+		assertTrue(this.test);
+		
+		//Boolean Assignment
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("Boolean a := true\na := false\n")), listener);
+		assertEquals(this.counter, 0);
+		this.interpreter.execute();
+		assertEquals(this.counter, 2);
+		
+		//Integer Assignment
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("Integer a := 5\na := 4\n")), listener);
+		assertEquals(this.counter, 0);
+		this.interpreter.execute();
+		assertEquals(this.counter, 2);
+		
+		//Loop
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("while(false)\n")), listener);
+		assertFalse(this.test);
+		this.interpreter.execute();
+		assertTrue(this.test);
+		
+		//Precondition Annotation
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("function Boolean test()\n_requires(true)\n{\n}\ntest()\n")), listener);
+		assertEquals(this.counter, 0);
+		this.interpreter.execute();
+		assertEquals(this.counter, 2);
+		
+		//Postcondition Annotation
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("function Boolean test()\n{\n}\n_ensures(true)\ntest()\n")), listener);
+		assertEquals(this.counter, 0);
+		this.interpreter.execute();
+		assertEquals(this.counter, 2);
+		
+		//Assertion Annotation
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("_assert(true)\n")), listener);
+		assertFalse(this.test);
+		this.interpreter.execute();
+		assertTrue(this.test);
+		
+		//Assumption Annotation
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("_assume(true)\n")), listener);
+		assertFalse(this.test);
+		this.interpreter.execute();
+		assertTrue(this.test);
+		
+		//Axiom Annotation
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("_axiom(true)\n")), listener);
+		assertFalse(this.test);
+		this.interpreter.execute();
+		assertTrue(this.test);
+		
+		//Condition
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("if(true) {\n}\n")), listener);
+		assertFalse(this.test);
+		this.interpreter.execute();
+		assertTrue(this.test);
+		
+		//Boolean ReturnStatement
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("function Boolean test() {\nreturn true\n}\ntest()\n")), listener);
+		assertEquals(this.counter, 0);
+		this.interpreter.execute();
+		assertEquals(this.counter, 2);
+		
+		//Integer ReturnStatement
+		this.reset(new Interpreter(TestASTProvider.getRootASTNode("function Integer test() {\nreturn 3\n}\ntest()\n")), listener);
+		assertEquals(this.counter, 0);
+		this.interpreter.execute();
+		assertEquals(this.counter, 2);
+	}
+	
 	@Test
 	public void testStatementExecuted() {
 		AbstractExecutionEventListener listener = new AbstractExecutionEventListener() {
 			public void statementExecuted(Statement statement) {
 				test = true;
+				counter++;
 			}
 		};
 		
-		this.reset(new Interpreter(TestASTProvider.getRootASTNode("Boolean a := true\n")), listener);
-		this.interpreter.execute();
-		assertTrue(this.test);
-		//this.reset(new Interpreter(TestASTProvider.getRootASTNode("Integer a := 5\n")), listener);
-		
+		this.testStatements(listener);
 	}
 	
 	@Test
@@ -38,12 +116,11 @@ public class InterpreterExecutionEventListenerTest {
 		AbstractExecutionEventListener listener = new AbstractExecutionEventListener() {
 			public void statementWillExecute(Statement statement) {
 				test = true;
+				counter++;
 			}
 		};
 		
-		this.reset(new Interpreter(TestASTProvider.getRootASTNode("Boolean a := true\n")), listener);
-		this.interpreter.execute();
-		assertTrue(this.test);
+		this.testStatements(listener);
 	}
 	
 	
@@ -64,5 +141,6 @@ public class InterpreterExecutionEventListenerTest {
 		this.interpreter = inter;
 		this.interpreter.addExecutionEventHandler(listener);
 		this.test = false;
+		this.counter = 0;
 	}
 }

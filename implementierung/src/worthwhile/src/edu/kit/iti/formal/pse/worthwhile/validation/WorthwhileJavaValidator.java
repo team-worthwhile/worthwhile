0b1ce674ff4018d6644215f68ca7051f33013977
Joachim <jspam@web.de>
2012-01-16 13:59:46 +0100

    [validator] Call typesystem from validator

--- implementierung/src/worthwhile/src/edu/kit/iti/formal/pse/worthwhile/validation/WorthwhileJavaValidator.java
+++ implementierung/src/worthwhile/src/edu/kit/iti/formal/pse/worthwhile/validation/WorthwhileJavaValidator.java
@@ -1,39 +1,49 @@
 package edu.kit.iti.formal.pse.worthwhile.validation;
 
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.validation.Check;
 
 import com.google.inject.Inject;
 
 import de.itemis.xtext.typesystem.ITypesystem;
+import edu.kit.iti.formal.pse.worthwhile.model.ast.ASTNode;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.FunctionDeclaration;
 
-
+/**
+ * Performs semantic validation on a Worthwhile program.
+ * 
+ */
 public class WorthwhileJavaValidator extends AbstractWorthwhileJavaValidator {
+	
+	/**
+	 * The language typesystem.
+	 */
+	@Inject
+	private ITypesystem ts;
 
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
-//		}
-//	}
-	
-	
+	/**
+	 * Checks if a function declaration contains a return statement on all code paths.
+	 * 
+	 * @param functionDeclaration
+	 *                The function declaration to check.
+	 */
 	@Check
-	public void checkFunctionDeclerationValidReturnStatment(FunctionDeclaration functionDeclaration) {
+	public final void checkFunctionDeclerationValidReturnStatment(final FunctionDeclaration functionDeclaration) {
 		ValidatorASTNodeVisitor validatorASTNodeVisitor = new ValidatorASTNodeVisitor();
 		validatorASTNodeVisitor.visit(functionDeclaration);
-		if(!validatorASTNodeVisitor.getValidReturnFound()) {
+		if (!validatorASTNodeVisitor.getValidReturnFound()) {
 			error("Function has no valid return statement.", null);
 		}
 	}
-	@Inject
-	private ITypesystem ts;
-	
-	@Check
-	public void checkTypesystemRules( EObject x ) {
-	  ts.checkTypesystemConstraints(x, this);
-	}
 
+	/**
+	 * Checks if the given AST node conforms to the typesystem rules.
+	 * 
+	 * @param node
+	 *                The AST node to check.
+	 */
+	@Check
+	public final void checkTypesystemRules(final ASTNode node) {
+		ts.checkTypesystemConstraints(node, this);
+	}
 
 }

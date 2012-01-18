0b3f5aea8899abd509c88b9ef31fcdb6eb46f0b9
Leon Handreke <leonh@ndreke.de>
2012-01-18 14:23:05 +0100

    [prover] Comment and clean up FreshVariableSetVisitor

--- implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/FreshVariableSetVisitor.java
+++ implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/FreshVariableSetVisitor.java
@@ -13,18 +13,37 @@ import edu.kit.iti.formal.pse.worthwhile.model.ast.VariableReference;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.util.AstNodeCloneHelper;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.visitor.HierarchialASTNodeVisitor;
 
+/**
+ * A visitor to replace all {@link VariableDeclaration} with new instances with the same name and type.
+ * 
+ * @author Leon Handreke
+ */
 public class FreshVariableSetVisitor extends HierarchialASTNodeVisitor {
 
+	/**
+	 * Map old variables to the new, fresh variables.
+	 * 
+	 * Use a LinkedHashMap here because the ordering of the keys is stable, which is needed to make the results
+	 * deterministic
+	 */
 	private Map<VariableDeclaration, VariableDeclaration> variableMap = new LinkedHashMap<VariableDeclaration, VariableDeclaration>();
 
+	/**
+	 * Stack to hand expressions back up the calling visit method.
+	 */
 	private Stack<Expression> substituteExpressionStack = new Stack<Expression>();
 
-	public Map<VariableDeclaration, VariableDeclaration> getVariableMap() {
+	/**
+	 * A mapping from the substituted to the substitute {@link VariableDeclaration}s.
+	 * 
+	 * @return the mapping from old to new variables
+	 */
+	public final Map<VariableDeclaration, VariableDeclaration> getVariableMap() {
 		return this.variableMap;
 	}
 
 	@Override
-	public void visit(final BinaryExpression b) {
+	public final void visit(final BinaryExpression b) {
 		b.getLeft().accept(this);
 		b.setLeft(this.substituteExpressionStack.pop());
 		b.getRight().accept(this);
@@ -33,20 +52,20 @@ public class FreshVariableSetVisitor extends HierarchialASTNodeVisitor {
 	}
 
 	@Override
-	public void visit(final Literal a) {
+	public final void visit(final Literal a) {
 		this.substituteExpressionStack.push(a);
 	}
 
 	@Override
-	public void visit(final UnaryExpression u) {
+	public final void visit(final UnaryExpression u) {
 		u.getOperand().accept(this);
 		u.setOperand(this.substituteExpressionStack.pop());
 		this.substituteExpressionStack.push(u);
 	}
 
 	@Override
-	public void visit(final VariableReference v) {
-		if(!this.variableMap.containsKey(v.getVariable())) {
+	public final void visit(final VariableReference v) {
+		if (!this.variableMap.containsKey(v.getVariable())) {
 			// if the VariableDeclaration was first seen, create a new one with the same name
 			VariableDeclaration newVariable = AstNodeCloneHelper.clone(v.getVariable());
 			newVariable.setInitialValue(null);

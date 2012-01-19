9b876195e545c20bb09782eaa7fa41f17e5803c0
Leon Handreke <leonh@ndreke.de>
2012-01-19 18:18:40 +0100

    [prover] Add package-info.java

---
+++ implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/package-info.java
@@ -0,0 +1,6 @@
+/**
+Prove annotations in a Worthwhile program for validity using an external prover program.
+@author Leon Handreke
+@author fabian
+*/
+package edu.kit.iti.formal.pse.worthwhile.prover;

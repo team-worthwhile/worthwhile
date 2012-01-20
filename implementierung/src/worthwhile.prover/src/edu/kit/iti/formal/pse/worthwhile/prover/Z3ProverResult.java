48cdedb4824ef0381084fc89792f2373dc30b901
Fabian <bafain@gmail.com>
2012-01-20 14:46:51 +0100

    [prover] Get Satisfiability from first (un-)sat line
    - therefore split prover output by newline and return Satisfiability as
      soon as a line is found that equals either unsat or sat
    - this implementation ignores everything else like WARNING: or model
      lines

--- implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/Z3ProverResult.java
+++ implementierung/src/worthwhile.prover/src/edu/kit/iti/formal/pse/worthwhile/prover/Z3ProverResult.java
@@ -17,10 +17,12 @@ public class Z3ProverResult extends ProverResult {
 
 	@Override
 	public final FormulaSatisfiability getSatisfiability() {
-		if (this.getOutput().startsWith("sat")) {
-			return FormulaSatisfiability.SATISFIABLE;
-		} else if (this.getOutput().startsWith("unsat")) {
-			return FormulaSatisfiability.UNSATISFIABLE;
+		for (final String s : this.getOutput().split("\n")) {
+			if (s.equals("sat")) {
+				return FormulaSatisfiability.SATISFIABLE;
+			} else if (s.equals("unsat")) {
+				return FormulaSatisfiability.UNSATISFIABLE;
+			}
 		}
 		// TODO: Do something even more intelligent here
 		// maybe some error checking, but how would we report the error?

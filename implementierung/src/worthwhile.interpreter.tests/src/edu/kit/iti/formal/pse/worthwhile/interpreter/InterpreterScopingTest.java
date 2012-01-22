8a178b36f14770b047cc313b8dcf34a46f636145
Stefan <sdderstefn@googlemail.com>
2012-01-22 01:40:37 +0100

    [interpreter.tests] Run formatter on InterpreterScopingTest.java.

--- implementierung/src/worthwhile.interpreter.tests/src/edu/kit/iti/formal/pse/worthwhile/interpreter/InterpreterScopingTest.java
+++ implementierung/src/worthwhile.interpreter.tests/src/edu/kit/iti/formal/pse/worthwhile/interpreter/InterpreterScopingTest.java
@@ -13,204 +13,162 @@ import edu.kit.iti.formal.pse.worthwhile.common.tests.TestASTProvider;
 
 public class InterpreterScopingTest {
 	private Interpreter interpreter;
-	
+
 	@Before
 	public void initializeTest() {
-		//this.setInterpreter("");
+		// this.setInterpreter("");
 	}
-	
+
 	public void setInterpreter(String programCode) {
-		this.interpreter = new Interpreter(TestASTProvider.getRootASTNode(programCode));
+		this.interpreter = new Interpreter(
+				TestASTProvider.getRootASTNode(programCode));
 	}
-	
+
 	public void assertVariableEqualsValue(String variableName, Value value) {
 		assertTrue(this.interpreter.getSymbol(variableName).equals(value));
 	}
-	
+
 	@Test
 	public void testWhileScopingWithInteger() {
-		this.setInterpreter(
-				"Integer a := 42" +
-				"\nInteger i := 0" +
-				"\nwhile(i < 10) {" +
-				"\ni := i + 1" +
-				"\nInteger a := 12" +
-				"\na := a - 1" +
-				"\n}" +
-				"\n");
+		this.setInterpreter("Integer a := 42" + "\nInteger i := 0"
+				+ "\nwhile(i < 10) {" + "\ni := i + 1" + "\nInteger a := 12"
+				+ "\na := a - 1" + "\n}" + "\n");
 		this.interpreter.execute();
-		this.assertVariableEqualsValue("a", new IntegerValue(new BigInteger("42")));
-		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger("10")));
+		this.assertVariableEqualsValue("a", new IntegerValue(new BigInteger(
+				"42")));
+		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger(
+				"10")));
 	}
-	
+
 	@Test
 	public void testWhileScopingWithBoolean() {
-		this.setInterpreter(
-				"Boolean a := true" +
-				"\nInteger i := 0" +
-				"\nwhile(i < 10) {" +
-				"\ni := i + 1" +
-				"\nInteger a := 12" +
-				"\na := a - 1" +
-				"\n}" +
-				"\n");
+		this.setInterpreter("Boolean a := true" + "\nInteger i := 0"
+				+ "\nwhile(i < 10) {" + "\ni := i + 1" + "\nInteger a := 12"
+				+ "\na := a - 1" + "\n}" + "\n");
 		this.interpreter.execute();
 		this.assertVariableEqualsValue("a", new BooleanValue(true));
-		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger("10")));
+		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger(
+				"10")));
 	}
-	
+
 	@Test
 	public void testWhileScopingWithIntegerArray() {
-		this.setInterpreter(
-				"Integer[] a := {42, 12, 100, 1}" +
-				"\nInteger i := 0" +
-				"\nwhile(i < 10) {" +
-				"\ni := i + 1" +
-				"\na[1] := 4" +
-				"\n}" +
-				"\n");
+		this.setInterpreter("Integer[] a := {42, 12, 100, 1}"
+				+ "\nInteger i := 0" + "\nwhile(i < 10) {" + "\ni := i + 1"
+				+ "\na[1] := 4" + "\n}" + "\n");
 		IntegerValue[] array = new IntegerValue[4];
 		array[0] = new IntegerValue(new BigInteger("42"));
 		array[1] = new IntegerValue(new BigInteger("12"));
 		array[2] = new IntegerValue(new BigInteger("100"));
 		array[3] = new IntegerValue(new BigInteger("1"));
 		this.interpreter.execute();
-		this.assertVariableEqualsValue("a", new CompositeValue<IntegerValue>(array));
-		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger("10")));
+		this.assertVariableEqualsValue("a", new CompositeValue<IntegerValue>(
+				array));
+		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger(
+				"10")));
 	}
-	
+
 	@Test
 	public void testWhileScopingWithBooleanArray() {
-		this.setInterpreter(
-				"Boolean[] a := {true, false, false, true}" +
-				"\nInteger i := 0" +
-				"\nwhile(i < 10) {" +
-				"\ni := i + 1" +
-				"\na[1] := true" +
-				"\n}" +
-				"\n");
+		this.setInterpreter("Boolean[] a := {true, false, false, true}"
+				+ "\nInteger i := 0" + "\nwhile(i < 10) {" + "\ni := i + 1"
+				+ "\na[1] := true" + "\n}" + "\n");
 		BooleanValue[] array = new BooleanValue[4];
 		array[0] = new BooleanValue(true);
 		array[1] = new BooleanValue(false);
 		array[2] = new BooleanValue(false);
 		array[3] = new BooleanValue(true);
 		this.interpreter.execute();
-		this.assertVariableEqualsValue("a", new CompositeValue<BooleanValue>(array));
-		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger("10")));
+		this.assertVariableEqualsValue("a", new CompositeValue<BooleanValue>(
+				array));
+		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger(
+				"10")));
 	}
-	
+
 	@Test
 	public void testIfScopingWithInteger() {
-		this.setInterpreter(
-				"Integer a := 42" +
-				"\nInteger i := 0" +
-				"\nif(i < 10) {" +
-				"\ni := 10" +
-				"\nInteger a := 12" +
-				"\na := a - 1" +
-				"\n}" +
-				"\n");
+		this.setInterpreter("Integer a := 42" + "\nInteger i := 0"
+				+ "\nif(i < 10) {" + "\ni := 10" + "\nInteger a := 12"
+				+ "\na := a - 1" + "\n}" + "\n");
 		this.interpreter.execute();
-		this.assertVariableEqualsValue("a", new IntegerValue(new BigInteger("42")));
-		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger("10")));
+		this.assertVariableEqualsValue("a", new IntegerValue(new BigInteger(
+				"42")));
+		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger(
+				"10")));
 	}
-	
+
 	@Test
 	public void testIfScopingWithBoolean() {
-		this.setInterpreter(
-				"Boolean a := true" +
-				"\nInteger i := 0" +
-				"\nwhile(i < 10) {" +
-				"\ni := i + 1" +
-				"\nInteger a := 12" +
-				"\na := a - 1" +
-				"\n}" +
-				"\n");
+		this.setInterpreter("Boolean a := true" + "\nInteger i := 0"
+				+ "\nwhile(i < 10) {" + "\ni := i + 1" + "\nInteger a := 12"
+				+ "\na := a - 1" + "\n}" + "\n");
 		this.interpreter.execute();
 		this.assertVariableEqualsValue("a", new BooleanValue(true));
-		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger("10")));
+		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger(
+				"10")));
 	}
-	
+
 	@Test
 	public void testIfScopingWithIntegerArray() {
-		this.setInterpreter(
-				"Boolean[] a := {42, 12, 100, 1}" +
-				"\nInteger i := 0" +
-				"\nif(i < 10) {" +
-				"\ni := 10" +
-				"\na[1] := true" +
-				"\n}" +
-				"\n");
+		this.setInterpreter("Boolean[] a := {42, 12, 100, 1}"
+				+ "\nInteger i := 0" + "\nif(i < 10) {" + "\ni := 10"
+				+ "\na[1] := true" + "\n}" + "\n");
 		IntegerValue[] array = new IntegerValue[4];
 		array[0] = new IntegerValue(new BigInteger("42"));
 		array[1] = new IntegerValue(new BigInteger("12"));
 		array[2] = new IntegerValue(new BigInteger("100"));
 		array[3] = new IntegerValue(new BigInteger("1"));
 		this.interpreter.execute();
-		this.assertVariableEqualsValue("a", new CompositeValue<IntegerValue>(array));
-		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger("10")));
+		this.assertVariableEqualsValue("a", new CompositeValue<IntegerValue>(
+				array));
+		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger(
+				"10")));
 	}
-	
+
 	@Test
 	public void testIfScopingWithBooleanArray() {
-		this.setInterpreter(
-				"Boolean[] a := {true, false, false, true}" +
-				"\nInteger i := 0" +
-				"\nif(i < 10) {" +
-				"\ni := 10" +
-				"\na[1] := true" +
-				"\n}" +
-				"\n");
+		this.setInterpreter("Boolean[] a := {true, false, false, true}"
+				+ "\nInteger i := 0" + "\nif(i < 10) {" + "\ni := 10"
+				+ "\na[1] := true" + "\n}" + "\n");
 		BooleanValue[] array = new BooleanValue[4];
 		array[0] = new BooleanValue(true);
 		array[1] = new BooleanValue(false);
 		array[2] = new BooleanValue(false);
 		array[3] = new BooleanValue(true);
 		this.interpreter.execute();
-		this.assertVariableEqualsValue("a", new CompositeValue<BooleanValue>(array));
-		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger("10")));
+		this.assertVariableEqualsValue("a", new CompositeValue<BooleanValue>(
+				array));
+		this.assertVariableEqualsValue("i", new IntegerValue(new BigInteger(
+				"10")));
 	}
-	
+
 	@Test
 	public void testFunctionScopingWithInteger() {
-		this.setInterpreter(
-				"function Integer test() {" +
-				"\nInteger a := 23" +
-				"\nreturn a" +
-				"\n}" +
-				"\nInteger a := 42" +
-				"\nInteger b := test()" +
-				"\n");
+		this.setInterpreter("function Integer test() {" + "\nInteger a := 23"
+				+ "\nreturn a" + "\n}" + "\nInteger a := 42"
+				+ "\nInteger b := test()" + "\n");
 		this.interpreter.execute();
-		this.assertVariableEqualsValue("a", new IntegerValue(new BigInteger("42")));
+		this.assertVariableEqualsValue("a", new IntegerValue(new BigInteger(
+				"42")));
 	}
-	
+
 	@Test
 	public void testFunctionScopingWithBoolean() {
-		this.setInterpreter(
-				"function Boolean test() {" +
-				"\nBoolean a := 23" +
-				"\nreturn a" +
-				"\n}" +
-				"\nBoolean a := 42" +
-				"\nBoolean b := test()" +
-				"\n");
+		this.setInterpreter("function Boolean test() {" + "\nBoolean a := 23"
+				+ "\nreturn a" + "\n}" + "\nBoolean a := 42"
+				+ "\nBoolean b := test()" + "\n");
 		this.interpreter.execute();
 		this.assertVariableEqualsValue("a", new BooleanValue(true));
 	}
-	
+
 	@Test
 	public void testFunctionScopingWithIntegerArray() {
-		this.setInterpreter(
-				"function Integer[] test() {" +
-				"\nInteger[] a := {21, 100, 13, 0}" +
-				"\nb := {1, 3, 101, 99}" +
-				"\nreturn a" +
-				"\n}" +
-				"\nInteger[] a := {42, 12, 100, 1}" +
-				"\nInteger[] b := {1, 2, 3, 4}" +
-				"\nInteger[] c := test()" +
-				"\n");
+		this.setInterpreter("function Integer[] test() {"
+				+ "\nInteger[] a := {21, 100, 13, 0}"
+				+ "\nb := {1, 3, 101, 99}" + "\nreturn a" + "\n}"
+				+ "\nInteger[] a := {42, 12, 100, 1}"
+				+ "\nInteger[] b := {1, 2, 3, 4}" + "\nInteger[] c := test()"
+				+ "\n");
 		IntegerValue[] array1 = new IntegerValue[4];
 		array1[0] = new IntegerValue(new BigInteger("42"));
 		array1[1] = new IntegerValue(new BigInteger("12"));
@@ -221,22 +179,20 @@ public class InterpreterScopingTest {
 		array2[1] = new IntegerValue(new BigInteger("3"));
 		array2[2] = new IntegerValue(new BigInteger("101"));
 		array2[3] = new IntegerValue(new BigInteger("99"));
-		this.assertVariableEqualsValue("a", new CompositeValue<IntegerValue>(array1));
-		this.assertVariableEqualsValue("b", new CompositeValue<IntegerValue>(array2));
+		this.assertVariableEqualsValue("a", new CompositeValue<IntegerValue>(
+				array1));
+		this.assertVariableEqualsValue("b", new CompositeValue<IntegerValue>(
+				array2));
 	}
-	
+
 	@Test
 	public void testFunctionScopingWithBooleanArray() {
-		this.setInterpreter(
-				"function Boolean[] test() {" +
-				"\nBoolean[] a := {true, true, true, false}" +
-				"\nb[0] := true" +
-				"\nreturn a" +
-				"\n}" +
-				"\nBoolean[] a := {true, false, false, true}" +
-				"\nBoolean[] b := {false, true, true, true}" +
-				"\nBoolean[] c := test()" +
-				"\n");
+		this.setInterpreter("function Boolean[] test() {"
+				+ "\nBoolean[] a := {true, true, true, false}"
+				+ "\nb[0] := true" + "\nreturn a" + "\n}"
+				+ "\nBoolean[] a := {true, false, false, true}"
+				+ "\nBoolean[] b := {false, true, true, true}"
+				+ "\nBoolean[] c := test()" + "\n");
 		BooleanValue[] array1 = new BooleanValue[4];
 		array1[0] = new BooleanValue(true);
 		array1[1] = new BooleanValue(false);
@@ -247,7 +203,9 @@ public class InterpreterScopingTest {
 		array2[1] = new BooleanValue(true);
 		array2[2] = new BooleanValue(true);
 		array2[3] = new BooleanValue(true);
-		this.assertVariableEqualsValue("a", new CompositeValue<BooleanValue>(array1));
-		this.assertVariableEqualsValue("b", new CompositeValue<BooleanValue>(array2));
+		this.assertVariableEqualsValue("a", new CompositeValue<BooleanValue>(
+				array1));
+		this.assertVariableEqualsValue("b", new CompositeValue<BooleanValue>(
+				array2));
 	}
 }

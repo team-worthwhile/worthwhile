278a3b83348b76b64f67d5d645d7aa9577d0ab1c
Fabian <bafain@gmail.com>
2012-01-24 11:28:08 +0100

    [interpreter.tests] Make TestExecutionListener inner class
    - TestExecutionListener is very specific to
      InterpreterExecutionEventListenerTest and only structures access to
      two attributes check and counter
    - TODO replace use of TestExecutionListener with
      TestStatementExecutionEventListener

--- implementierung/src/worthwhile.interpreter.tests/src/edu/kit/iti/formal/pse/worthwhile/interpreter/TestExecutionListener.java
+++
@@ -1,6 +0,0 @@
-package edu.kit.iti.formal.pse.worthwhile.interpreter;
-
-public class TestExecutionListener extends AbstractExecutionEventListener {
-	public boolean check = false;
-	public int counter = 0;
-}

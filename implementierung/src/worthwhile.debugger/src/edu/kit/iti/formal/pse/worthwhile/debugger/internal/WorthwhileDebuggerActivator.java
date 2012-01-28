55492653349fa9b365889bd50c94aad66d6598db
Joachim <jspam@web.de>
2012-01-28 22:36:48 +0100

    [debugger] Remove unneeded activator class

--- implementierung/src/worthwhile.debugger/src/edu/kit/iti/formal/pse/worthwhile/debugger/internal/WorthwhileDebuggerActivator.java
+++
@@ -1,8 +0,0 @@
-package edu.kit.iti.formal.pse.worthwhile.debugger.internal;
-
-import org.eclipse.core.runtime.Plugin;
-import org.osgi.framework.BundleActivator;
-
-public class WorthwhileDebuggerActivator extends Plugin implements BundleActivator {
-
-}

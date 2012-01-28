f6360ba99b3fe2ff7843c394b1605bd1f90f3456
Joachim <jspam@web.de>
2012-01-28 13:53:07 +0100

    [formatter] Apply formatter to formatter class

--- implementierung/src/worthwhile/src/edu/kit/iti/formal/pse/worthwhile/formatting/WorthwhileFormatter.java
+++ implementierung/src/worthwhile/src/edu/kit/iti/formal/pse/worthwhile/formatting/WorthwhileFormatter.java
@@ -11,31 +11,30 @@ import org.eclipse.xtext.util.Pair;
 
 /**
  * This class contains custom formatting description.
- *
- * see : http://www.eclipse.org/Xtext/documentation/latest/xtext.html#formatting
- * on how and when to use it
- *
- * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an
- * example
+ * 
+ * see : http://www.eclipse.org/Xtext/documentation/latest/xtext.html#formatting on how and when to use it
+ * 
+ * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an example
  */
 public class WorthwhileFormatter extends AbstractDeclarativeFormatter {
 
-    @Override
-    protected final void configureFormatting(final FormattingConfig c) {
-	WorthwhileGrammarAccess f = (WorthwhileGrammarAccess) getGrammarAccess();
-	for (Pair<Keyword, Keyword> pair : f.findKeywordPairs("{", "}")) {
-	    c.setIndentation(pair.getFirst(), pair.getSecond());
-	    c.setLinewrap(1).after(pair.getFirst());
-	    c.setLinewrap(1).before(pair.getSecond());
-	    c.setLinewrap(1).after(pair.getSecond());
+	@Override
+	protected final void configureFormatting(final FormattingConfig c) {
+		WorthwhileGrammarAccess f = (WorthwhileGrammarAccess) getGrammarAccess();
+		for (Pair<Keyword, Keyword> pair : f.findKeywordPairs("{", "}")) {
+
+			c.setIndentation(pair.getFirst(), pair.getSecond());
+			c.setLinewrap(1).after(pair.getFirst());
+			c.setLinewrap(1).before(pair.getSecond());
+			c.setLinewrap(1).after(pair.getSecond());
+		}
+		for (Keyword comma : f.findKeywords(",")) {
+			c.setNoLinewrap().before(comma);
+			c.setNoSpace().before(comma);
+			c.setLinewrap().after(comma);
+		}
+		c.setLinewrap(0, 1, 2).before(f.getSL_COMMENTRule());
+		c.setLinewrap(0, 1, 2).before(f.getML_COMMENTRule());
+		c.setLinewrap(0, 1, 1).after(f.getML_COMMENTRule());
 	}
-	for (Keyword comma : f.findKeywords(",")) {
-	    c.setNoLinewrap().before(comma);
-	    c.setNoSpace().before(comma);
-	    c.setLinewrap().after(comma);
-	}
-	c.setLinewrap(0, 1, 2).before(f.getSL_COMMENTRule());
-	c.setLinewrap(0, 1, 2).before(f.getML_COMMENTRule());
-	c.setLinewrap(0, 1, 1).after(f.getML_COMMENTRule());
-    }
 }

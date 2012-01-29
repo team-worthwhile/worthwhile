1393726271b675495a6c828ca1070a12ef34a983
Fabian <bafain@gmail.com>
2012-01-29 18:02:25 +0100

    [prover.tests] Make DivisionByZeroAssertionInserterTest parameterized

--- implementierung/src/worthwhile.prover.tests/src/edu/kit/iti/formal/pse/worthwhile/prover/DivisionByZeroAssertionInserterTest.java
+++ implementierung/src/worthwhile.prover.tests/src/edu/kit/iti/formal/pse/worthwhile/prover/DivisionByZeroAssertionInserterTest.java
@@ -1,7 +1,13 @@
 package edu.kit.iti.formal.pse.worthwhile.prover;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import edu.kit.iti.formal.pse.worthwhile.common.tests.TestASTProvider;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Program;
@@ -11,51 +17,83 @@ import edu.kit.iti.formal.pse.worthwhile.model.ast.Program;
  * 
  * @author Leon Handreke
  */
+@RunWith(Parameterized.class)
 public class DivisionByZeroAssertionInserterTest {
-
 	/**
 	 * Test if division by a zero literal causes the proof to fail.
 	 */
-	@Test
-	public final void testDivisionByZeroLiteral() {
-		Program p = TestASTProvider.getRootASTNode("Integer a := 1/0\n_assert(true)\n");
-		SpecificationChecker checker = new SpecificationChecker();
-		Validity validity = checker.checkProgram(p);
-		Assert.assertEquals(Validity.INVALID, validity);
-	}
+	private static final String DIVISIONBYZEROLITERAL = "Integer a := 1/0\n_assert(true)\n";
 
 	/**
 	 * Test if division by a variable set to zero causes the proof to fail.
 	 */
-	@Test
-	public final void testDivisionByZeroVariable() {
-		Program p = TestASTProvider.getRootASTNode("Integer a := 0\nInteger b := 42 / a\n_assert(true)\n");
-		SpecificationChecker checker = new SpecificationChecker();
-		Validity validity = checker.checkProgram(p);
-		Assert.assertEquals(Validity.INVALID, validity);
-	}
+	private static final String DIVISIONBYZEROVARIABLE = "Integer a := 0\nInteger b := 42 / a\n_assert(true)\n";
 
 	/**
 	 * Test if division by a variable that was previously set to zero causes the proof to succeed like it should.
 	 */
-	@Test
-	public final void testDivisionByMultipleZeroVariables() {
-		Program p = TestASTProvider
-		                .getRootASTNode("Integer a := 2\nInteger b := 42 / a\na := 0\nb := 42 / 1\n_assert(true)\n");
-		SpecificationChecker checker = new SpecificationChecker();
-		Validity validity = checker.checkProgram(p);
-		Assert.assertEquals(Validity.VALID, validity);
-	}
+	private static final String DIVISIONBYMULTIPLEZEROVARIABLES = "Integer a := 2\nInteger b := 42 / a\na := 0\nb := 42 / 1\n_assert(true)\n";
 
 	/**
 	 * Test if Divisions by zero inside the condition of a Conditional cause the proof to fail.
 	 */
+	private static final String DIVISIONBYZEROCONDITIONAL = "Integer a := 0\nif(10 / a = 2) {\n    a := 0\n    Integer b := 42 / 1\n}\n_assert(true)\n";
+
+	/**
+	 * The {@link SpecificationChecker} whose {@link Program} validity checking is tested.
+	 */
+	private SpecificationChecker checker;
+
+	/**
+	 * The {@link Program} for which the {@link DivisionByZeroAssertionInserterTest#checker}'s validity checking is
+	 * tested.
+	 */
+	private Program testProgram;
+
+	/**
+	 * The expected value for the {@link DivisionByZeroAssertionInserterTest#testProgram}'s validity.
+	 */
+	private Validity expectedValidity;
+
+	/**
+	 * 
+	 * @return a {@link Collection} of test parameters, in which several
+	 *         {@link DivisionByZeroAssertionInserterTest#testProgram}s are assigned an
+	 *         {@link DivisionByZeroAssertionInserterTest#expectedValidity}
+	 */
+	@Parameters
+	public static Collection<Object[]> testData() {
+		return Arrays.asList(new Object[][] {
+		                { TestASTProvider.getRootASTNode(DIVISIONBYZEROLITERAL), Validity.INVALID },
+		                { TestASTProvider.getRootASTNode(DIVISIONBYZEROVARIABLE), Validity.INVALID },
+		                { TestASTProvider.getRootASTNode(DIVISIONBYMULTIPLEZEROVARIABLES), Validity.VALID },
+		                { TestASTProvider.getRootASTNode(DIVISIONBYZEROCONDITIONAL), Validity.INVALID } });
+	}
+
+	/**
+	 * Initializes the test parameters and the to be used {@link SpecificationChecker}.
+	 * 
+	 * @param testProgram
+	 *                the {@link Program} whose {@link Validity} is to be checked
+	 * @param expectedValidity
+	 *                the <code>Validity</code> expected for the given <code>testProgram</code>
+	 * 
+	 * @see DivisionByZeroAssertionInserterTest#testTestProgramValidity()
+	 */
+	public DivisionByZeroAssertionInserterTest(final Program testProgram, final Validity expectedValidity) {
+		this.checker = new SpecificationChecker();
+
+		this.testProgram = testProgram;
+		this.expectedValidity = expectedValidity;
+	}
+
+	/**
+	 * Asserts that the {@link DivisionByZeroAssertionInserterTest#checker} returns the expected
+	 * {@link DivisionByZeroAssertionInserterTest#expectedValidity} for the
+	 * {@link DivisionByZeroAssertionInserterTest#testProgram}.
+	 */
 	@Test
-	public final void testDivisionByZeroInConditional() {
-		Program p = TestASTProvider.getRootASTNode("Integer a := 0\nif(10 / a = 2) {\n    a := 0\n"
-		                + "    Integer b := 42 / 1\n}\n_assert(true)\n");
-		SpecificationChecker checker = new SpecificationChecker();
-		Validity validity = checker.checkProgram(p);
-		Assert.assertEquals(Validity.INVALID, validity);
+	public final void testTestProgramValidity() {
+		Assert.assertEquals(this.expectedValidity, this.checker.checkProgram(this.testProgram));
 	}
 }

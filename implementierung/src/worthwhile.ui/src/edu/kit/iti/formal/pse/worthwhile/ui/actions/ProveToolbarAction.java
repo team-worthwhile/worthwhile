2a1aa12e229865aa06fae36f7167aaf2b97192da
Joachim <jspam@web.de>
2012-02-22 00:47:28 +0100

    [ui:proving] Fix #85: Use standard launch shortcut for proving

---
+++ implementierung/src/worthwhile.ui/src/edu/kit/iti/formal/pse/worthwhile/ui/actions/ProveToolbarAction.java
@@ -0,0 +1,20 @@
+package edu.kit.iti.formal.pse.worthwhile.ui.actions;
+
+import org.eclipse.debug.ui.actions.AbstractLaunchToolbarAction;
+
+/**
+ * A toolbar action for proving a program.
+ * 
+ * @author Joachim
+ * 
+ */
+public class ProveToolbarAction extends AbstractLaunchToolbarAction {
+
+	/**
+	 * Creates a new instance of the {@code ProveToolbarAction} class.
+	 */
+	public ProveToolbarAction() {
+		super("edu.kit.iti.formal.pse.worthwhile.ui.launchGroup.prove");
+	}
+
+}

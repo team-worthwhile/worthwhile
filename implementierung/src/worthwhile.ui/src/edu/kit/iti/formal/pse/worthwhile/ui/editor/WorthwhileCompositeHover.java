8bcc6588da1655c7a5aa39d30de16c7094351908
Joachim <jspam@web.de>
2012-02-22 13:14:43 +0100

    [ui] Break lines in marker tooltips (follow-up of #45)

--- implementierung/src/worthwhile.ui/src/edu/kit/iti/formal/pse/worthwhile/ui/editor/WorthwhileCompositeHover.java
+++ implementierung/src/worthwhile.ui/src/edu/kit/iti/formal/pse/worthwhile/ui/editor/WorthwhileCompositeHover.java
@@ -5,13 +5,19 @@ import java.util.List;
 import org.eclipse.jface.text.ITextHover;
 import org.eclipse.xtext.ui.editor.hover.DefaultCompositeHover;
 
+/**
+ * A composite hover that adds the {@link WorthwhileMarkerHover} to the list of hovers.
+ * 
+ * @author Joachim
+ * 
+ */
 public class WorthwhileCompositeHover extends DefaultCompositeHover {
 
 	@Override
-        protected final List<ITextHover> createHovers() {
-	        List<ITextHover> hovers = super.createHovers();
-	        hovers.add(new WorthwhileMarkerHover());
-	        return hovers;
-        }
+	protected final List<ITextHover> createHovers() {
+		List<ITextHover> hovers = super.createHovers();
+		hovers.add(new WorthwhileMarkerHover());
+		return hovers;
+	}
 
 }
--- implementierung/src/worthwhile.ui/src/edu/kit/iti/formal/pse/worthwhile/ui/editor/WorthwhileMarkerHover.java
+++ implementierung/src/worthwhile.ui/src/edu/kit/iti/formal/pse/worthwhile/ui/editor/WorthwhileMarkerHover.java
@@ -1,12 +1,16 @@
 package edu.kit.iti.formal.pse.worthwhile.ui.editor;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.IAnnotationHover;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.xtext.ui.XtextUIMessages;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.hover.ProblemAnnotationHover;
 
@@ -15,13 +19,38 @@ import com.google.common.collect.Lists;
 /**
  * A hover provider that displays hover information on Worthwhile markers.
  * 
+ * More or less shamelessly copied from Xtext, because we need to override the private methods {@code splitInfo} and
+ * {@code isHandled}.
+ * 
+ * Changes:
+ * <ul>
+ * <li>Handling Worthwhile annotations in addition to the Xtext ones</li>
+ * <li>Replacing newlines by &lt;br&gt; in text hover, thus enabling line breaks there</li>
+ * </ul>
+ * 
+ * http://git.eclipse.org/c/tmf/org.eclipse.xtext.git/tree/plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext
+ * /ui/editor/hover/ProblemAnnotationHover.java?id=v2.0.1}
+ * 
  * @author Joachim
  * 
  */
 public class WorthwhileMarkerHover extends ProblemAnnotationHover implements IAnnotationHover {
 
-	// These two methods are shamelessly copied from AbstractProblemHover because isHandled() is only protected
-	// since Xtext 2.1
+	/**
+	 * The maximum width of a message, messages are line-wrapped when exceeding this number of characters per line.
+	 */
+	private static final int MSG_WIDTH = 100;
+
+	// IAnnotationHover
+	@Override
+	public final String getHoverInfo(final ISourceViewer sourceViewer, final int lineNumber) {
+		Object o = getHoverInfoInternal(sourceViewer, lineNumber, -1);
+		if (o != null) {
+			return o.toString().replace("<br>", "\n");
+		}
+		return null;
+	}
+
 	@Override
 	public final List<Annotation> getAnnotations(final int lineNumber, final int offset) {
 		if (getAnnotationModel() == null) {
@@ -56,6 +85,60 @@ public class WorthwhileMarkerHover extends ProblemAnnotationHover implements IAn
 		return result;
 	}
 
+	@Override
+	protected final String formatInfo(final Collection<String> messages) {
+		final StringBuffer buffer = new StringBuffer();
+		if (messages.size() > 1) {
+			buffer.append(XtextUIMessages.AbstractHover_MultipleMarkers);
+			final Iterator<String> e = messages.iterator();
+			while (e.hasNext()) {
+				splitInfo("- " + e.next() + "<br>", buffer);
+			}
+			buffer.deleteCharAt(buffer.length() - 1);
+		} else if (messages.size() == 1) {
+			splitInfo(messages.iterator().next(), buffer);
+		}
+		return buffer.toString();
+	}
+
+	/**
+	 * Splits a message at MAX_MSG_WIDTH chars.
+	 * 
+	 * @param message
+	 *                The message to split
+	 * @param buffer
+	 *                The buffer to write the message
+	 * @return The message, again (I don't know why).
+	 */
+	private String splitInfo(final String message, final StringBuffer buffer) {
+		// Replace newlines by <br>. Don't use <br /> here, Eclipse does not understand that.
+		// Split message at <br> so that the splitting point is always seeked from the beginning of a line
+		String[] messages = message.replace("\n", "<br>").split("<br>");
+		List<String> splitMessages = new ArrayList<String>();
+
+		for (String msg : messages) {
+			String prefix = "";
+			String splitMessage = "";
+			int pos;
+			do {
+				pos = msg.indexOf(" ", MSG_WIDTH);
+				if (pos > -1) {
+					splitMessage += prefix + msg.substring(0, pos) + "<br>";
+					msg = msg.substring(pos);
+					prefix = "  ";
+				} else {
+					splitMessage += prefix + msg;
+				}
+			} while (pos > -1);
+
+			splitMessages.add(splitMessage);
+		}
+
+		// Join previously split strings and return them
+		buffer.append(join(splitMessages, "<br>"));
+		return buffer.toString();
+	}
+
 	/**
 	 * Partly copied from {@code AbstractProblemHover}.
 	 * 
@@ -72,4 +155,31 @@ public class WorthwhileMarkerHover extends ProblemAnnotationHover implements IAn
 		return null != annotation && !annotation.isMarkedDeleted()
 		                && (Arrays.asList(handledAnnotations).contains(annotation.getType()));
 	}
+
+	/**
+	 * Joins a collection of strings to a string.
+	 * 
+	 * from http://stackoverflow.com/questions/187676/java-equivalents-of-c-sharp-string-format-and-string-join
+	 * 
+	 * @param <T>
+	 *                The type of the collection.
+	 * @param s
+	 *                The collection of strings
+	 * @param delimiter
+	 *                The delimiter
+	 * @return The joined string.
+	 */
+	private static <T> String join(final Collection<T> s, final String delimiter) {
+		StringBuilder builder = new StringBuilder();
+		Iterator<T> iter = s.iterator();
+		while (iter.hasNext()) {
+			builder.append(iter.next());
+			if (!iter.hasNext()) {
+				break;
+			}
+			builder.append(delimiter);
+		}
+		return builder.toString();
+	}
+
 }

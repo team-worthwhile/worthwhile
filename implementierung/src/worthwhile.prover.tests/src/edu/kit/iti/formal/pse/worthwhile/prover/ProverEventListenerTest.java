464dc181aca40bc02e853936b78adeac5363ee15
Joachim <jspam@web.de>
2012-03-05 00:49:48 +0100

    [prover.tests] Add tests for guard assertions

--- implementierung/src/worthwhile.prover.tests/src/edu/kit/iti/formal/pse/worthwhile/prover/ProverEventListenerTest.java
+++ implementierung/src/worthwhile.prover.tests/src/edu/kit/iti/formal/pse/worthwhile/prover/ProverEventListenerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import edu.kit.iti.formal.pse.worthwhile.common.tests.TestASTProvider;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Expression;
+import edu.kit.iti.formal.pse.worthwhile.model.ast.FunctionCall;
+import edu.kit.iti.formal.pse.worthwhile.model.ast.IntegerLiteral;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Program;
 
 /**
@@ -46,8 +48,8 @@ public class ProverEventListenerTest {
 		 */
 		class TestDistributorCountingProverEventListener extends CountingProverEventListener {
 			@Override
-			public void programVerified(final Program program, final Validity validity, final Expression formula,
-			                final ProverResult proverResult) {
+			public void programVerified(final Program program, final Validity validity,
+			                final Expression formula, final ProverResult proverResult) {
 				super.programVerified(program, validity, formula, proverResult);
 				Assert.assertEquals(Validity.VALID, validity);
 			}
@@ -70,5 +72,62 @@ public class ProverEventListenerTest {
 
 		Assert.assertEquals(2, firstListener.getProgramVerifiedCounter());
 		Assert.assertEquals(1, secondListener.getProgramVerifiedCounter());
+	}
+
+	@Test
+	public final void testDivisorNotZeroVerified() {
+		/**
+		 * Test-specific event listener class that also checks whether the supplied statement is the correct one.
+		 */
+		class TestDistributorCountingProverEventListener extends CountingProverEventListener {
+			
+			public boolean checked = false;
+			
+			@Override
+			public void divisorNotZeroVerified(final Expression divisor, final Validity validity,
+			                final Expression formula, final ProverResult proverResult) {
+
+				this.checked = true;
+				Assert.assertTrue(divisor instanceof IntegerLiteral);
+			}
+		}
+		
+		TestDistributorCountingProverEventListener listener = new TestDistributorCountingProverEventListener();
+
+		SpecificationChecker checker = new SpecificationChecker(new Z3Prover(5000));
+		checker.addProverEventListener(listener);
+		
+		Program p = TestASTProvider.getRootASTNode("Integer i := 2/2\n");
+		checker.checkProgram(p);
+
+		Assert.assertEquals(true, listener.checked);
+	}
+	
+	@Test
+	public final void testFunctionCallPreconditionValidVerified() {
+		/**
+		 * Test-specific event listener class
+		 */
+		class TestDistributorCountingProverEventListener extends CountingProverEventListener {
+			
+			public boolean checked = false;
+			
+			@Override
+			public void functionCallPreconditionValidVerified(final FunctionCall functionCall, final Validity validity,
+			                final Expression formula, final ProverResult proverResult) {
+
+				this.checked = true;
+			}
+		}
+		
+		TestDistributorCountingProverEventListener listener = new TestDistributorCountingProverEventListener();
+
+		SpecificationChecker checker = new SpecificationChecker(new Z3Prover(5000));
+		checker.addProverEventListener(listener);
+		
+		Program p = TestASTProvider.getRootASTNode("function Integer test()\n_requires true\n{return 42\n}\nInteger i := test()\n");
+		checker.checkProgram(p);
+
+		Assert.assertEquals(true, listener.checked);
 	}
 }

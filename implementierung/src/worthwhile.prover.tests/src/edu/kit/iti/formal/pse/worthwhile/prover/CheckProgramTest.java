a41562c7bd1a0e0f4ed6477ace7e7b78fbd62707
Fabian <bafain@gmail.com>
2012-03-04 02:43:24 +0100

    [prover.tests] Add annotated functions (invalid_)array_max
    - invalid_array_max is wrongly proven correct

--- implementierung/src/worthwhile.prover.tests/src/edu/kit/iti/formal/pse/worthwhile/prover/CheckProgramTest.java
+++ implementierung/src/worthwhile.prover.tests/src/edu/kit/iti/formal/pse/worthwhile/prover/CheckProgramTest.java
@@ -59,7 +59,8 @@ public class CheckProgramTest {
 		String[] testPrograms = { "test-division", "test-faculty", "test-even", "test-max", "test-clone",
 		                "test-arrayindexassignment", "test-incrementfunction", "DebuggerDemo",
 		                "smtlib-keywords", "test-qexpr-condition", "test-github-issue-93", "test-fibonacci",
-		                "bubblesort2", "multiplication", "test-github-issue-108" };
+		                "bubblesort2", "multiplication", "test-github-issue-108", "array_max",
+		                "invalid_array_max" };
 
 		for (String fileName : testPrograms) {
 			// Load test program and look for EXPECTED:???

ef9373ecd34ac058395dfe2ac078e18e16842c92
Fabian <bafain@gmail.com>
2012-03-10 18:43:23 +0100

    Fix checkstyle errors in (Grammar|Validator)Test
    - make Test classes final
    - remove unused import
    - add missing javadoc
    - apply Worthwhile formatter

--- implementierung/src/worthwhile.common.tests/src/edu/kit/iti/formal/pse/worthwhile/common/tests/GrammarTest.java
+++ implementierung/src/worthwhile.common.tests/src/edu/kit/iti/formal/pse/worthwhile/common/tests/GrammarTest.java
@@ -6,48 +6,52 @@ import org.junit.Test;
 
 /**
  * Some tests for the Worthwhile grammar.
- *
+ * 
  */
-public class GrammarTest {
+public final class GrammarTest {
 
 	/**
 	 * Tests that the spacing around the minus sign is irrelevant for the grammar.
 	 */
 	@Test
 	public void testMinusSign() {
-		String testAST = "function Integer y() {\nreturn 42\n}\n" + "Integer i\n"
+		String testAST = "function Integer y() {\nreturn 42\n}\n"
+		                + "Integer i\n"
 		                + "_assert (i-1) = 0\n_assert (i--1) = 0\n_assert (i-i) = 0\n_assert (i--i) = 0\n"
 		                + "_assert (i-y()) = 0\n_assert (i--y()) = 0\n"
 		                + "_assert (i - 1) = 0\n_assert (i - - 1) = 0\n_assert (i - i) = 0\n_assert (i - - i) = 0\n"
 		                + "_assert (i - y()) = 0\n_assert (i - - y()) = 0\n";
-		
+
 		assertEquals(0, TestASTProvider.getErrorCount(testAST));
 	}
-	
+
 	/**
-	 * Tests that the return value reference in a function’s postcondition accepts an array index
-	 * if the function returns an array.
+	 * Tests that the return value reference in a function’s postcondition accepts an array index if the function
+	 * returns an array.
 	 */
 	@Test
 	public void testReturnValueReferenceIndex() {
 		String testAST = "function Integer[] x()\n _ensures _return[0] = 1\n{\nreturn {1}\n}\n";
-		
+
 		TestASTProvider.getRootASTNode(testAST);
 		assertEquals(0, TestASTProvider.getErrorCount(testAST));
 	}
-	
+
 	/**
 	 * Tests that the spacing in conditional expressions is irrelevant.
 	 */
 	@Test
 	public final void testConditionalExpressionSpacing() {
-		String testAST = "if (false) { }\nelse { }\n"
-				+ "if (false) \n { \n } \n else \n { \n } \n"
-				+ "if (false) { \n } \n else { \n }\n";
-		
-		TestUtils.assertErrorCountEquals(0,  TestASTProvider.loadProgram(testAST));
+		String testAST = "if (false) { }\nelse { }\n" + "if (false) \n { \n } \n else \n { \n } \n"
+		                + "if (false) { \n } \n else { \n }\n";
+
+		TestUtils.assertErrorCountEquals(0, TestASTProvider.loadProgram(testAST));
 	}
 
+	/**
+	 * Tests that the parser reports an error when a <code>return</code> statement is placed outside a function
+	 * declaration.
+	 */
 	@Test
 	public void testReturnStatementOnlyFunction() {
 		String testProgram = "function Integer fun() {\n" + "return true\n" + "}\n" + "return true \n";
--- implementierung/src/worthwhile.common.tests/src/edu/kit/iti/formal/pse/worthwhile/common/tests/ValidatorTest.java
+++ implementierung/src/worthwhile.common.tests/src/edu/kit/iti/formal/pse/worthwhile/common/tests/ValidatorTest.java
@@ -17,7 +17,6 @@ import de.itemis.xtext.typesystem.testing.XTextTestCase;
 import edu.kit.iti.formal.pse.worthwhile.WorthwhileStandaloneSetup;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.Assignment;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.FunctionDeclaration;
-import edu.kit.iti.formal.pse.worthwhile.model.ast.ReturnStatement;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.ReturnValueReference;
 import edu.kit.iti.formal.pse.worthwhile.model.ast.VariableDeclaration;
 
@@ -25,97 +24,99 @@ import edu.kit.iti.formal.pse.worthwhile.model.ast.VariableDeclaration;
  * Test class for the validator.
  * 
  */
-public class ValidatorTest extends XTextTestCase {
+public final class ValidatorTest extends XTextTestCase {
 
-    @Test
-    public void testEmptyArrayLiteral() {
-	String testProgram = "Integer[]  k := {}\n";
-	Assert.assertEquals(0, TestASTProvider.getErrorCount(testProgram));
-    }
-
-    @Test
-    public void testTypesOfParams() {
-	String testProgram = "Integer a := true\n" + "a := false \n";
-
-	initialize(testProgram);
-	assertConstraints(allIssues.forType(VariableDeclaration.class).sizeIs(1));
-
-    }
-
-    @Test
-    public void testReturnValueReferenceOnlyPostcondition() {
-	String testProgram = "function Integer fun()\n" + "_requires _return > 4\n" + "_ensures _return > 4\n" + "{\n"
-		+ "return 0\n" + "}\n";
-
-	initialize(testProgram);
-	assertConstraints(allIssues.forType(ReturnValueReference.class).sizeIs(1));
-    }
-
-    @Test
-    public void testFunctionDeclarationValidReturnStatement() {
-	String testProgram = "function Integer fun() {\n" + "if (true) { \n" + "return 4\n" + "}\n" + "}\n";
-	initialize(testProgram);
-	assertConstraints(allIssues.forType(FunctionDeclaration.class).sizeIs(1));
-    }
-
-    @Test
-    public void testReturnValueRefwithIndex() {
-	String testProgram = "function Integer fun()\n" + "_ensures _return[0] = 5\n" + "{\n" + "return 0\n" + "}\n"
-		+ "function Boolean[] fun2()\n" + "_ensures _return[0] = false\n" + "{\n" + "return {false}\n" + "}\n";
-	Assert.assertEquals(1, TestASTProvider.getErrorCount(testProgram));
-    }
-
-    @Test
-    public void testFunctionParametersNotModified() {
-	String testProgram = "function Integer fun(Integer a, Boolean b) {\n" + "a := 8\n" + "Boolean c := b\n"
-		+ "return 0\n" + "}\n";
-	initialize(testProgram);
-	assertConstraints(allIssues.forType(Assignment.class).sizeIs(1));
-    }
-
-    /**
-     * Tests that undeclared functions are not checked for their parameter count.
-     */
-    @Test
-    public final void testUndeclaredFunctionParameterCountCheck() {
-	String testProgram = "Integer i := g(1)\nInteger j := g()\n";
-	TestUtils.assertErrorCountEquals(2, TestASTProvider.loadProgram(testProgram));
-    }
-
-    @Test
-    public void testFunctionCallParameter() {
-	String testProgram = "function Integer fun(Integer a, Boolean b) {\n" + "return 4\n" + "}\n"
-		+ "Integer k := fun(5,true)\n" + "k := fun(true)\n" + "k := fun(true,true)\n";
-	initialize(testProgram);
-	assertConstraints(allIssues.sizeIs(2));
-    }
-
-    @Test
-    public void testQuantifiedExpressionOnlyAnnotation() {
-	String testProgram = "_assume forall Boolean a : a = true\n" + "Boolean d := forall Integer k : k > 0\n";
-	initialize(testProgram);
-	assertConstraints(allIssues.sizeIs(1));
-    }
-
-    @Test
-    public void testArrayLiteralnotInArrayLiteral() {
-	String testProgram = "Integer[] a := {{},5}\n" + "Boolean[] b := {true, {}}\n";
-	initialize(testProgram);
-	assertConstraints(allIssues.sizeIs(4));
-    }
-
-    private void initialize(String parseText) {
-	XtextResource resource = (XtextResource) TestASTProvider.loadProgram(parseText);
-
-	Assert.assertTrue(resource.getErrors().isEmpty());
-
-	Injector guiceInjector = new WorthwhileStandaloneSetup().createInjectorAndDoEMFRegistration();
-	IResourceValidator validator = guiceInjector.getInstance(IResourceValidator.class);
-	List<Issue> validationErrors = validator.validate(resource, CheckMode.ALL, null);
-	List<String> messages = new ArrayList<String>();
-	for (Issue issue : validationErrors) {
-	    messages.add(issue.getMessage());
+	@Test
+	public void testEmptyArrayLiteral() {
+		String testProgram = "Integer[]  k := {}\n";
+		Assert.assertEquals(0, TestASTProvider.getErrorCount(testProgram));
 	}
-	allIssues = new IssueCollection(resource, validationErrors, messages);
-    }
+
+	@Test
+	public void testTypesOfParams() {
+		String testProgram = "Integer a := true\n" + "a := false \n";
+
+		initialize(testProgram);
+		assertConstraints(allIssues.forType(VariableDeclaration.class).sizeIs(1));
+
+	}
+
+	@Test
+	public void testReturnValueReferenceOnlyPostcondition() {
+		String testProgram = "function Integer fun()\n" + "_requires _return > 4\n" + "_ensures _return > 4\n"
+		                + "{\n" + "return 0\n" + "}\n";
+
+		initialize(testProgram);
+		assertConstraints(allIssues.forType(ReturnValueReference.class).sizeIs(1));
+	}
+
+	@Test
+	public void testFunctionDeclarationValidReturnStatement() {
+		String testProgram = "function Integer fun() {\n" + "if (true) { \n" + "return 4\n" + "}\n" + "}\n";
+		initialize(testProgram);
+		assertConstraints(allIssues.forType(FunctionDeclaration.class).sizeIs(1));
+	}
+
+	@Test
+	public void testReturnValueRefwithIndex() {
+		String testProgram = "function Integer fun()\n" + "_ensures _return[0] = 5\n" + "{\n" + "return 0\n"
+		                + "}\n" + "function Boolean[] fun2()\n" + "_ensures _return[0] = false\n" + "{\n"
+		                + "return {false}\n" + "}\n";
+		Assert.assertEquals(1, TestASTProvider.getErrorCount(testProgram));
+	}
+
+	@Test
+	public void testFunctionParametersNotModified() {
+		String testProgram = "function Integer fun(Integer a, Boolean b) {\n" + "a := 8\n" + "Boolean c := b\n"
+		                + "return 0\n" + "}\n";
+		initialize(testProgram);
+		assertConstraints(allIssues.forType(Assignment.class).sizeIs(1));
+	}
+
+	/**
+	 * Tests that undeclared functions are not checked for their parameter count.
+	 */
+	@Test
+	public final void testUndeclaredFunctionParameterCountCheck() {
+		String testProgram = "Integer i := g(1)\nInteger j := g()\n";
+		TestUtils.assertErrorCountEquals(2, TestASTProvider.loadProgram(testProgram));
+	}
+
+	@Test
+	public void testFunctionCallParameter() {
+		String testProgram = "function Integer fun(Integer a, Boolean b) {\n" + "return 4\n" + "}\n"
+		                + "Integer k := fun(5,true)\n" + "k := fun(true)\n" + "k := fun(true,true)\n";
+		initialize(testProgram);
+		assertConstraints(allIssues.sizeIs(2));
+	}
+
+	@Test
+	public void testQuantifiedExpressionOnlyAnnotation() {
+		String testProgram = "_assume forall Boolean a : a = true\n"
+		                + "Boolean d := forall Integer k : k > 0\n";
+		initialize(testProgram);
+		assertConstraints(allIssues.sizeIs(1));
+	}
+
+	@Test
+	public void testArrayLiteralnotInArrayLiteral() {
+		String testProgram = "Integer[] a := {{},5}\n" + "Boolean[] b := {true, {}}\n";
+		initialize(testProgram);
+		assertConstraints(allIssues.sizeIs(4));
+	}
+
+	private void initialize(String parseText) {
+		XtextResource resource = (XtextResource) TestASTProvider.loadProgram(parseText);
+
+		Assert.assertTrue(resource.getErrors().isEmpty());
+
+		Injector guiceInjector = new WorthwhileStandaloneSetup().createInjectorAndDoEMFRegistration();
+		IResourceValidator validator = guiceInjector.getInstance(IResourceValidator.class);
+		List<Issue> validationErrors = validator.validate(resource, CheckMode.ALL, null);
+		List<String> messages = new ArrayList<String>();
+		for (Issue issue : validationErrors) {
+			messages.add(issue.getMessage());
+		}
+		allIssues = new IssueCollection(resource, validationErrors, messages);
+	}
 }
